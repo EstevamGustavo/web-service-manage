6f67175cae741b42993e51a14383938e69118903
Gustavo Estevam <gustavo.estevam@outlook.com>
2019-11-26 09:31:15 -0300

    conflict fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-gcloud function deploy pi_project --trigger-http --project pi-project
-=======
+
 gcloud functions deploy piWebService --runtime nodejs8 --trigger-http --project pi-project
->>>>>>> 7652a8038fed50bf34fe8ceda6b743e2d316367e
+
